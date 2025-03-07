0e7bca96b4871116f2df6cc1c82ccc4737e5a41f
SuYihang <1935304975@qq.com>
2025-03-07 13:36:09 +0800

    优化代码结构

--- 07-矩阵按键/src/Int/Int_KeyMatrix.c
+++ 07-矩阵按键/src/Int/Int_KeyMatrix.c
@@ -1,144 +1,58 @@
 #include <STC89C5xRC.H>
 #include "Int_KeyMatrix.h"
 
-u8 Int_keyMatrix_CheckSW(){
+u8 Int_keyMatrix_CheckSW()
+{
 
-    // 检测第一行
-    P2 = 0xFE;
-    if (P24 == 0) {
-        Com_Util_Delay_1MS(10);
-        if(P24 == 0) {
-            while(P24 == 0);
-            return 5;
+    u8 i, j;
+    u8 lines[] = {0xFE, 0xFD, 0xFB, 0xF7};
+    for (i = 0; i < 4; i++) {
+        P2 = lines[i];
+        for (j = 0; j < 4; j++) {
+            if ((P2 & (0x10 << j)) == 0x00) {
+                Com_Util_Delay_1MS(10);
+                if ((P2 & (0x10 << j)) == 0x00) {
+                    while ((P2 & (0x10 << j)) == 0x00);
+                    return 5 + j + 4 * i;
+                }
+            }
         }
     }
 
-    if (P25 == 0) {
-        Com_Util_Delay_1MS(10);
-        if(P25 == 0) {
-            while(P25 == 0);
-            return 6;
-        } 
-    }
+    //     // 检测第一行
+    //     P2 = lines[i];
+    //     if (P24 == 0) {
+    //         Com_Util_Delay_1MS(10);
+    //         if (P24 == 0) {
+    //             while (P24 == 0);
+    //             return 5 + 4 * i;
+    //         }
+    //     }
 
-    if (P26 == 0) {
-        Com_Util_Delay_1MS(10);
-        if(P26 == 0) {
-            while(P26 == 0);
-            return 7;
-        } 
-    }
+    //     if (P25 == 0) {
+    //         Com_Util_Delay_1MS(10);
+    //         if (P25 == 0) {
+    //             while (P25 == 0);
+    //             return 6 + 4 * i;
+    //         }
+    //     }
 
-    if (P27 == 0) {
-        Com_Util_Delay_1MS(10);
-        if(P27 == 0) {
-            while(P27 == 0);
-            return 8; 
-        }  
-    }
+    //     if (P26 == 0) {
+    //         Com_Util_Delay_1MS(10);
+    //         if (P26 == 0) {
+    //             while (P26 == 0);
+    //             return 7 + 4 * i;
+    //         }
+    //     }
 
-    // 检测第二行
-    P2 = 0xFD;
-    if (P24 == 0) {
-        Com_Util_Delay_1MS(10);
-        if(P24 == 0) {
-            while(P24 == 0);
-            return 9;
-        }
-    }
-
-    if (P25 == 0) {
-        Com_Util_Delay_1MS(10);
-        if(P25 == 0) {
-            while(P25 == 0);
-            return 10;
-        }  
-    }
-
-    if (P26 == 0) {
-        Com_Util_Delay_1MS(10);
-        if(P26 == 0) {
-            while(P26 == 0);
-            return 11;
-        } 
-    }
-
-    if (P27 == 0) {
-        Com_Util_Delay_1MS(10);
-        if(P27 == 0) {
-            while(P27 == 0);
-            return 12;
-        } 
-    }
-
-    // 检测第三行
-    P2 = 0xFB;
-    if (P24 == 0) {
-        Com_Util_Delay_1MS(10);
-        if(P24 == 0) {
-            while(P24 == 0);
-            return 13;
-        } 
-    }
-
-    if (P25 == 0) {
-        Com_Util_Delay_1MS(10);
-        if(P25 == 0) {
-            while(P25 == 0);
-            return 14;
-        } 
-    }
-
-    if (P26 == 0) {
-        Com_Util_Delay_1MS(10);
-        if(P26 == 0) {
-            while(P26 == 0);
-            return 15;
-        }  
-    }
-
-    if (P27 == 0) {
-        Com_Util_Delay_1MS(10);
-        if(P27 == 0) {
-            while(P27 == 0);
-            return 16;
-        } 
-    }
-
-    // 检测第四行
-    P2 = 0xF7;
-    if (P24 == 0) {
-        Com_Util_Delay_1MS(10);
-        if(P24 == 0) {
-            while(P24 == 0);
-            return 17;
-        }
-    }
-
-    if (P25 == 0) {
-        Com_Util_Delay_1MS(10);
-        if(P25 == 0) {
-            while(P25 == 0);
-            return 18;
-        } 
-    }
-
-    if (P26 == 0) {
-        Com_Util_Delay_1MS(10);
-        if(P26 == 0) {
-            while(P26 == 0);
-            return 19;
-        }  
-    }
-
-    if (P27 == 0) {
-        Com_Util_Delay_1MS(10);
-        if(P27 == 0) {
-            while(P27 == 0);
-            return 20;
-        } 
-    }
+    //     if (P27 == 0) {
+    //         Com_Util_Delay_1MS(10);
+    //         if (P27 == 0) {
+    //             while (P27 == 0);
+    //             return 8 + 4 * i;
+    //         }
+    //     }
+    // }
 
     return 0;
-
 }
